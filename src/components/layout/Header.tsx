00a84a0ed97aca77e7b917e2a9dee3570b69af71
olhkyle <kylekwon.dev@gmail.com>
2025-08-01 17:42:46 +0900

    style : change `Header` style as `sticky`

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import { url } from "@/constants";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center mx-auto px-4 min-h-[60px]">
+    <header className="sticky top-0 flex justify-between items-center mx-auto px-4 min-h-[60px]">
       <h1 className="p-2 text-xl font-bold cursor-pointer">
         <Link href={url.HOME}>Spatial</Link>
       </h1>
